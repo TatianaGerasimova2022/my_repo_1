596ac8414ecd759db37cb84cbdb216a452f4811e
TatianaGerasimova2022 <106173658+TatianaGerasimova2022@users.noreply.github.com>
2022-05-24 19:35:13 +0300

    add2

--- HWorld.py
+++ HWorld.py
@@ -2,3 +2,4 @@ Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AM
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print('hello world')
 print('hello world123')
+print('hello world1234567')
